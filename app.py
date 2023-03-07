d62670a1a26e85d420db399df8276eb9299d1a6a
Akvilion <ianko.andrii@gmail.com>
2023-03-07 14:27:49 +0200

    4

--- app.py
+++ app.py
@@ -7,29 +7,29 @@ import customExeptions as cex
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def parser():
 
     url: str = request.args.get("url", "")
-    
-    try:
-        parser = Parser(url)
-        csv_data = parser.csvFile()
-    except cex.ParserExeption:
-        return jsonify({"exeption": "Something wrong with Parser"}), 200
-    except cex.ConvertToCSVExeption:
-        return jsonify({"exeption": "Something wrong with CSV converter"}), 200
-    except cex.RequestExeption:
-        return jsonify({"exeption": "Something wrong with URL request"}), 200
-    except:
-        return jsonify({"exeption": "Something go wrong completely"}), 200
-
-    response = make_response(csv_data)
-
-    response.headers['Content-Disposition'] = 'attachment; filename=data.csv'
-    response.headers['Content-Type'] = 'text/csv'
-
-    return response
+    if url:
+        try:
+            parser = Parser(url)
+            csv_data = parser.csvFile()
+        except cex.ParserExeption:
+            return jsonify({"exeption": "Something wrong with Parser"}), 200
+        except cex.ConvertToCSVExeption:
+            return jsonify({"exeption": "Something wrong with CSV converter"}), 200
+        except cex.RequestExeption:
+            return jsonify({"exeption": "Something wrong with URL request"}), 200
+        except:
+            return jsonify({"exeption": "Something go wrong completely"}), 200
+        else:
+            response = make_response(csv_data)
+            response.headers['Content-Disposition'] = 'attachment; filename=data.csv'
+            response.headers['Content-Type'] = 'text/csv'
+            return response  
+    else:
+        return jsonify({"URL is needed": "plese specify URL"})
 
 
 if __name__ == "__main__":
